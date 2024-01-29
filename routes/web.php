c6ff5634f90ba31cda51ddaf6b8b8d210d54bf16
David wanjohi <davidwagura83@gmail.com>
2024-01-29 11:39:38 +0300

    creat controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::post('/register', function () {
-    return 'thank you';
-});
+Route::post('/register', [UserController::class, 'register']);
